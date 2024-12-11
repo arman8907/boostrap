b818a52a96839c697067e6e6df1aed2b418ffe0a
arman8907 <mkrtchyanarman018@gmail.com>
2024-12-11 09:44:26 +0400

    boostrap

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var button = document.getElementById('btn')
+
+btn.addEventListener('click' , function() {
+    document.getElementById('https://www.youtube.com/watch?v=vLnPwxZdW4Y').style.background = 'black'
